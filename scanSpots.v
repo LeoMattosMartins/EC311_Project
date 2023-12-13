066b0107a6b946a837d0cde7158a2ffb03b4ca8a
Dom Murphy <dommurphy1229@gmail.com>
2023-12-13 07:09:28 -0500

    Add files via upload

---
+++ scanSpots.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Dominic Murphy
+// 
+// Create Date: 12/13/2023 04:14:26 AM
+// Design Name: 
+// Module Name: scanSpots
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module scanSpots(
+        //have 8 directions and scan every one of them each in your checkCHECK bc u can have multiple checks that way
+    );
+endmodule
