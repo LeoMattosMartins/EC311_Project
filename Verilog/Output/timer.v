a8a49018d5dbcf243bdfef7651b9d327f718886f
ralexk <153458965+ralexk@users.noreply.github.com>
2023-12-13 23:54:05 -0500

    Update timer.v

--- Verilog/Output/timer.v
+++ Verilog/Output/timer.v
@@ -30,90 +30,106 @@ module timer(
     );
     
     reg [3:0] sec2Black = 4'b1001;
-    reg [2:0] sec1Black = 3'b011;
-    reg [2:0] minBlack = 3'b101;
+    reg [2:0] sec1Black = 3'b101;
+    reg [2:0] minBlack = 3'b100;
     reg [3:0] sec2White = 4'b1001;
-    reg [2:0] sec1White = 3'b011;
-    reg [2:0] minWhite = 3'b101;
+    reg [2:0] sec1White = 3'b101;
+    reg [2:0] minWhite = 3'b100;
     
     
     always@(posedge clk, posedge rst) begin
         if(rst) begin
             sec2Black = 4'b1001;
-            sec1Black = 3'b011;
-            minBlack = 3'b101;
+            sec1Black = 3'b101;
+            minBlack = 3'b100;
             sec2White = 4'b1001;
-            sec1White = 3'b011;
-            minWhite = 3'b101;
+            sec1White = 3'b101;
+            minWhite = 3'b100;
         end
         
         else begin
             if (moveData[13] == 0) begin
-                if (sec2White <= 9 && sec1White>=5 && minWhite>=0) begin
-                    sec2Black = 4'b1001;
-                    sec1Black = 3'b011;
-                    minBlack = 3'b101;
+                if (sec2White > 0 && sec1White>0 && minWhite>0) begin
+                    sec2Black = sec2Black;
+                    sec1Black = sec1Black;
+                    minBlack = minBlack;
                     sec2White = sec2White - 1;
-                    sec1White = 3'b011;
-                    minWhite = 3'b101;              
+                    sec1White = sec1White;
+                    minWhite = minWhite;              
                 end
-                else if (sec2White == 0 && sec1White>=5 && minWhite>=0) begin
-                    sec2Black = 4'b1001;
-                    sec1Black = 4'b1001;
-                    minBlack = 3'b101;
-                    sec2White = sec2White - 1;
-                    sec1White = 3'b011;
-                    minWhite = 3'b101;
-                end
-                else if (sec2White == 0 && sec1White == 0 && minWhite>=0) begin
-                    sec2Black = 4'b1001;
-                    sec1Black = 3'b011;
-                    minBlack = 3'b101;
+                else if (sec2White == 0 && sec1White>0 && minWhite>0) begin
+                    sec2Black = sec2Black;
+                    sec1Black = sec1Black;
+                    minBlack = minBlack;
                     sec2White = 4'b1001;
-                    sec1White = 3'b011;
+                    sec1White = sec1White - 1;
+                    minWhite = minWhite;
+                end
+                else if (sec2White == 0 && sec1White == 0 && minWhite>0) begin
+                    sec2Black = sec2Black;
+                    sec1Black = sec1Black;
+                    minBlack = minBlack;
+                    sec2White = 4'b1001;
+                    sec1White = 3'b101;
                     minWhite = minWhite - 1;
                 end
                 else if (sec2White == 0 && sec1White == 0 && minWhite == 0) begin
+                    sec2Black = 4'b0;
+                    sec1Black = 3'b0;
+                    minBlack = 3'b0;
+                    sec2White = 4'b0;
+                    sec1White = 3'b0;
+                    minWhite = 3'b0;
+                end
+                else begin
                     sec2Black = 4'b1001;
-                    sec1Black = 3'b011;
-                    minBlack = 3'b101;
+                    sec1Black = 3'b101;
+                    minBlack = 3'b100;
                     sec2White = 4'b1001;
-                    sec1White = 3'b011;
-                    minWhite = 3'b101;
+                    sec1White = 3'b101;
+                    minWhite = 3'b100;
                 end
             end
             else if (moveData[13]) begin
-                if (sec2Black <= 9 && sec1Black>=5 && minBlack>=0) begin
+                if (sec2Black >0 && sec1Black>0 && minBlack>0) begin
                     sec2Black = sec2Black - 1;
-                    sec1Black = 3'b011;
-                    minBlack = 3'b101;
-                    sec2White = 4'b1001;
-                    sec1White = 3'b011;
-                    minWhite = 3'b101;              
+                    sec1Black = sec1Black;
+                    minBlack = minBlack;
+                    sec2White = sec2White;
+                    sec1White = sec1White;
+                    minWhite = minWhite;              
                 end
-                else if (sec2Black == 0 && sec1Black>=5 && minBlack>=0) begin
+                else if (sec2Black == 0 && sec1Black>0 && minBlack>0) begin
                     sec2Black = 4'b1001;
                     sec1Black = sec1Black - 1;
-                    minBlack = 3'b101;
-                    sec2White = 4'b1001;
-                    sec1White = 3'b011;
-                    minWhite = 3'b101;
+                    minBlack = minBlack;
+                    sec2White = sec2White;
+                    sec1White = sec1White;
+                    minWhite = minWhite;
                 end
-                else if (sec2Black == 0 && sec1Black == 0 && minBlack>=0) begin
+                else if (sec2Black == 0 && sec1Black == 0 && minBlack>0) begin
                     sec2Black = 4'b1001;
-                    sec1Black = 3'b011;
+                    sec1Black = 3'b101;
                     minBlack = minBlack - 1;
-                    sec2White = 4'b1001;
-                    sec1White = 3'b011;
-                    minWhite = 3'b101;
+                    sec2White = sec2White;
+                    sec1White = sec1White;
+                    minWhite = minWhite;
                 end
                 else if (sec2Black == 0 && sec1Black == 0 && minBlack == 0) begin
+                    sec2Black = 4'b0;
+                    sec1Black = 3'b0;
+                    minBlack = 3'b0;
+                    sec2White = 4'b0;
+                    sec1White = 3'b0;
+                    minWhite = 3'b0;
+                end
+                else begin 
                     sec2Black = 4'b1001;
-                    sec1Black = 3'b011;
-                    minBlack = 3'b101;
+                    sec1Black = 3'b101;
+                    minBlack = 3'b100;
                     sec2White = 4'b1001;
-                    sec1White = 3'b011;
-                    minWhite = 3'b101;
+                    sec1White = 3'b101;
+                    minWhite = 3'b100;
                 end
             end
         
